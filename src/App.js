127a7a91da2673ac00294d75d5b252039284f56c
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2023-03-25 23:30:52 -0300

    Load components on App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import Presentation from "./components/main-components/Presentation.jsx";
+import React from "react";
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
+import { theme } from "./data/theme.js";
+import NavBar from "./components/NavBar.jsx";
+import Main from "./components/Main.jsx";
+import Footer from "./components/Footer.jsx";
 
-function App() {
+export default function App() {
   return (
-    <Presentation/>
+    <CssVarsProvider theme={theme}>
+      <NavBar/>
+      <Main/>
+      <Footer/>
+    </CssVarsProvider>
   );
 }
-
-export default App;
